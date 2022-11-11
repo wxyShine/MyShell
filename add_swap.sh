dfb536fc08de156c710cc1f5dc4a0819120a2e50
wxyShine <wxyrrcj@gmail.com>
2022-11-11 15:03:22 +0800

    Update add_swap.sh

--- add_swap.sh
+++ add_swap.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
-#Blog:https://wxy97.com/
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
 
+# fonts color
 Green="\033[32m"
+Red="\033[31m"
+Yellow="\033[33m"
+GreenBG="\033[42;37m"
+RedBG="\033[41;37m"
 Font="\033[0m"
-Red="\033[31m" 
+# fonts color
+
 
 #root权限
 root_need(){
@@ -22,42 +29,42 @@ ovz_no(){
 }
 
 add_swap(){
-echo -e "${Green}请输入需要添加的swap，建议为内存的2倍！(MB)${Font}"
-read -p "请输入swap数值:" swapsize
+echo -e "${Green}请输入需要添加的 Swap 值,单位为 MB ,建议为内存的 2 倍.(例如输入:4096)${Font}"
+read -p "请输入 Swap 数值:" swapsize
 
 #检查是否存在swapfile
-grep -q "swapfile" /etc/fstab
+grep -q "swap" /etc/fstab
 
 #如果不存在将为其创建swap
 if [ $? -ne 0 ]; then
-	echo -e "${Green}swapfile未发现，正在为其创建swapfile${Font}"
-	fallocate -l ${swapsize}M /swapfile
+	echo -e "${Green}swapfile 未发现，正在为其创建 swapfile${Font}"
+    dd if=/dev/zero of=/swapfile bs=1M count=${swapsize}
 	chmod 600 /swapfile
 	mkswap /swapfile
 	swapon /swapfile
 	echo '/swapfile none swap defaults 0 0' >> /etc/fstab
-         echo -e "${Green}swap创建成功，并查看信息：${Font}"
+         echo -e "${Green}swap 创建成功，并查看信息：${Font}"
          cat /proc/swaps
          cat /proc/meminfo | grep Swap
 else
-	echo -e "${Red}swapfile已存在，swap设置失败，请先运行脚本删除swap后重新设置！${Font}"
+	echo -e "${Red}swapfile 已存在，swap 设置失败，请先运行脚本删除 swap 后重新设置！${Font}"
 fi
 }
 
 del_swap(){
 #检查是否存在swapfile
-grep -q "swapfile" /etc/fstab
+grep -q "swap" /etc/fstab
 
 #如果存在就将其移除
 if [ $? -eq 0 ]; then
-	echo -e "${Green}swapfile已发现，正在将其移除...${Font}"
-	sed -i '/swapfile/d' /etc/fstab
+	echo -e "${Green}swapfile 已发现，正在将其移除...${Font}"
+	sed -i '/swap/d' /etc/fstab
 	echo "3" > /proc/sys/vm/drop_caches
 	swapoff -a
 	rm -f /swapfile
-    echo -e "${Green}swap已删除！${Font}"
+    echo -e "${Green}swap 已删除！${Font}"
 else
-	echo -e "${Red}swapfile未发现，swap删除失败！${Font}"
+	echo -e "${Red}swapfile 未发现，swap 删除失败！${Font}"
 fi
 }
 
@@ -67,7 +74,7 @@ root_need
 ovz_no
 clear
 echo -e "———————————————————————————————————————"
-echo -e "${Green}Linux VPS一键添加/删除swap脚本${Font}"
+echo -e "${Green}一键添加/删除swap脚本${Font}"
 echo -e "${Green}1、添加swap${Font}"
 echo -e "${Green}2、删除swap${Font}"
 echo -e "———————————————————————————————————————"
